98407adb2f419f00d4d09ff7d6aa335d7836c21c
Pse1234 <78938133+Pse1234@users.noreply.github.com>
2023-02-19 16:16:53 +0100

    Initial commit

---
+++ src/home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(page_title="Segmentation RFM", layout="wide")
+st.title("Segmentation RFM of our business :sunglasses:")
+
+def main():
+    html_temp = """<script type='module' src='https://prod-uk-a.online.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js'></script><tableau-viz id='tableau-viz' src='https://prod-uk-a.online.tableau.com/t/segmentationrfm/views/segmentation_clientelle/Repartitionparrainage' width='1440' height='724' hide-tabs toolbar='bottom' ></tableau-viz>"""
+    components.html(html_temp, width=1440, height=724)
+if __name__ == "__main__":    
+    main()
+
+    
